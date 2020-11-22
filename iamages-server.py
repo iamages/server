e47117635746461470c2effa7c049f577ece9444
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2020-11-22 10:42:32 +0800

    🐞One more shot at CORS fix.

--- iamages-server.py
+++ iamages-server.py
@@ -113,6 +113,7 @@ def check_private_file(FileID, UserID):
 class BaseRequestHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "Accept")
     def options(self):
         self.set_status(204)
         self.finish()
